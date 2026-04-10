235873482c56809d32576ec335df61c5e7cfe105
bonieky <bonieky@gmail.com>
2026-04-10 17:43:05 -0300

    feat: create ping route

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,9 @@ import {
 
 const router = Router();
 
+router.get('/ping', (req, res) => {
+  res.json({ pong: true, v: 1 });
+});
 router.get('/tasks', getTasks);
 router.get('/tasks/:id', getTaskById);
 router.post('/tasks', createTask);
